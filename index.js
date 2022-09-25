ce671351ecc3a9f7a2a8222a497e2540583187e5
Argent <jrargent42@gmail.com>
2022-09-25 15:42:32 -0400

    initial set up and notes. init npm, install npm inquierer 8.4.2

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+//const generateFile = require('./src/markdown-template.js');
+
+
+/* section for asking user following questions:
+
+1. What is your GitHub username?
+2. What is your email address?
+3. What is your project's name?
+4. Please write a short description of your project.
+5. What command should be run to install dependencies? (npm i?)
+6. What command should be run to run tests? (npm test?)
+7. What does the user need to know about using the repo?
+8. What does the user need to know about contributing to the repo?
+
+*/
